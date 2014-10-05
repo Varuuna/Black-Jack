86af1917005bcca1ebae5038bd25e8ec11f58df0
Fredrik Andreasson <medumee@gmail.com>
2014-10-05 20:29:31 +0200

    Application

---
+++ Black_Jack_3/Deck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Black_Jack_3
+{
+	public class Deck
+	{
+		Random random = new Random();
+		public List<Card> deck = new List<Card>();
+		public Card card { get; set; }
+
+		// Creates all the cards and adds them to the deck.
+		public Deck()
+		{
+			for (int i = 1; i < 14; i++)
+			{
+				Card card = new Card()
+				{
+					Value = i,
+					Suit = Color.Spades
+				};
+				deck.Add(card);
+			}
+
+			for (int i = 1; i < 14; i++)
+			{
+				Card card = new Card()
+				{
+					Value = i,
+					Suit = Color.Hearts
+				};
+				deck.Add(card);
+
+			}
+
+			for (int i = 1; i < 14; i++)
+			{
+				Card card = new Card()
+				{
+					Value = i,
+					Suit = Color.Clubs
+				};
+				deck.Add(card);
+
+			}
+
+			for (int i = 1; i < 14; i++)
+			{
+				Card card = new Card()
+				{
+					Value = i,
+					Suit = Color.Diamonds
+				};
+				deck.Add(card);
+
+			}
+		}
+
+		// Randomly generates a card from the deck.
+		public Card GetCard()
+		{
+			int card = random.Next(0, 52);
+			return deck[card];
+		}
+
+	}
+}
